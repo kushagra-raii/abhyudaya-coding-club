bd9ba7fd2f2e932346ddfa91cbca41238b2139e7
kushagra-raii <kushagrarai83@gmail.com>
2024-03-08 11:08:50 +0530

    config: tailwind
    Added color for background, border and heading etc.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "rich-blue": {
+          bg: "#0F1437",
+          bgDark: "#080D51",
+          border: "#194EEF",
+        },
+        primary: {
+          heading: "#EBFF00",
+        },
+      },
     },
   },
   plugins: [],
