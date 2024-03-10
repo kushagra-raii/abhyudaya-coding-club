6d6a30752c05ce0f87ab03f26716987c893241f1
kushagra-raii <kushagrarai83@gmail.com>
2024-03-10 19:10:25 +0530

    Nav Responsive for mobile view

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import svvvLogo from "@/../public/assets/header/SVVV.svg";
 import Image from "next/image";
 import { navData } from "../../../public/data/navData";
 import Link from "next/link";
+import { FiAlignJustify, FiX } from "react-icons/fi";
+import { FaTimes } from "react-icons/fa";
 const Navbar = () => {
+  const [navOpen, setNavOpen] = useState(false);
+  const toggleModel = () => {
+    setNavOpen(!navOpen)
+  }
   return (
-    <nav className="bg-rich-blue-bg opacity-95">
+    <nav className="sm:bg-rich-blue-bg bg-[#111644ee] fixed sm:static w-full z-20 sm:opacity-95  ">
       <div className="w-11/12 mx-auto flex items-center justify-between">
         <div className="py-2">
           <Image src={svvvLogo} className="" alt="Logo" height={80} />
@@ -21,7 +29,25 @@ const Navbar = () => {
             </Link>
           ))}
         </div>
-        <div></div>
+        <div className=" sm:invisible relative">
+          <button onClick={toggleModel}>
+            {!navOpen && <FiAlignJustify color="white" size={35} />}
+            {navOpen && <FaTimes color="#ddd" size={35}/>}  
+          </button>
+          {
+            navOpen && <div className="absolute  right-4 flex flex-col" >
+            {navData?.map((item, idx) => (
+              <Link
+                href={item?.path}
+                key={idx}
+                className="px-5 py-1 border-b bg-[#0f1437b0] text-[#eee]  hover:text-[#fff]  text-nowrap"
+              >
+                {item?.title}
+              </Link>
+            ))}
+            </div>
+          }
+        </div>
       </div>
     </nav>
   );
