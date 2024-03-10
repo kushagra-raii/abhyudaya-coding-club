40716c03ec9c3f8634ea942569f5d63410b33f1b
kushagra-raii <kushagrarai83@gmail.com>
2024-03-10 11:48:18 +0530

    fix: faculty section
    Added dynamic style for top mangagement and faculty

--- src/components/FacultyCard.tsx
+++ src/components/FacultyCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import cx from "classnames";
 import { turret } from "@/app/(pages)/page";
 
 interface FacultyCardProp {
@@ -7,12 +8,22 @@ interface FacultyCardProp {
   isTopMgmt: boolean;
 }
 
+
+
+
 const FacultyCard = ({ person,isTopMgmt }: FacultyCardProp) => {
   const { img, name, post } = person;
+  const imgClass = cx({
+    "mb-2":true,
+    "min-w-80":isTopMgmt,
+  })
+  
+
+
   return (
     <div className="flex flex-col items-center w-54 xl:w-64 gap-1">
-      <div className="">
-      <Image src={img} className="mb-2 " alt="" />
+      <div>
+      <Image src={img} className={imgClass} alt="" />
       </div>
       <h2 className={` ${turret.className} text-2xl font-semibold`}>{name}</h2>
       <p>{post}</p>
--- src/components/FacultySection.tsx
+++ src/components/FacultySection.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import FacultyCard from './FacultyCard'
 import SectionDividerLine from './SectionDividerLine'
 import { turret } from '@/app/(pages)/page'
+import cx from "classnames"
 type Person  = {
     id: number;
     img: string;
@@ -15,15 +16,21 @@ type FacultySectionProps = {
 
 const FacultySection = ({faculties}:FacultySectionProps) => {
   const topManagement = faculties[0].post === "Vice Chancellor";
- 
+  const cardsClass = cx({
+    "flex":true,
+    "flex-wrap":true,
+    "gap-16":true,
+    "justify-center":true,
+    "gap-32":topManagement,
+  })
   return (
     <section className=" w-11/12 mx-auto flex flex-col items-center gap-16  ">
-    <h1 className={` text-4xl font-bold ${turret.className}`}>
+    <h1 className={` text-4xl gap font-bold ${turret.className}`}>
       {
         topManagement ? "VC & HOD" : "Faculty coordinator and mentor"
       }
     </h1>
-    <div className="flex flex-wrap gap-16 justify-center">
+    <div className={cardsClass}>
       {faculties?.map((person) => (
         <FacultyCard key={person.id} person={person} isTopMgmt={topManagement} />
       ))}
